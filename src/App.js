d472f33741a43a3c9245775efce11a4c3fe89565
RomanSyrko <syrko.roman@gmail.com>
2022-09-08 19:09:45 +0300

    Finally

--- src/App.js
+++ src/App.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import Sidebar from "./Components/Sidebar";
+import Main from "./Components/Main";
+import './App.css'
 
 const App = () => {
+
+    const [notes, setNotes] = useState(
+        localStorage.notes ? JSON.parse(localStorage.notes) : []
+    );
+    const [activeNote, setActiveNote] = useState(0);
+
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    const onAddNote = (e) => {
+        e.preventDefault()
+        const newNote = {
+            title: "Name",
+            body: "",
+            completed: false,
+            lastModified: Date.now(),
+            id: Math.random() * 1000
+        };
+
+        setNotes([newNote, ...notes]);
+        setActiveNote(newNote.id);
+    };
+
+    const onDeleteNote = (noteId) => {
+        setNotes(notes.filter(({id}) => id !== noteId));
+    };
+
+    const onCheckNote = (item) => {
+        const updateCompleted = notes.map((note) => {
+            if (note.id === item.id) {
+                return {
+                    ...item,
+                    completed: !item.completed
+                };
+            }
+            return note;
+        });
+        setNotes(updateCompleted)
+    };
+
+    const onUpdateNote = (updatedNote) => {
+        const updatedNotesArr = notes.map((note) => {
+            if (note.id === updatedNote.id) {
+                return updatedNote;
+            }
+            return note;
+        });
+        setNotes(updatedNotesArr);
+    };
+
+    const getActiveNote = () => {
+        return notes.find(({id}) => id === activeNote);
+    };
+
     return (
-        <div>
-          
+        <div className="App">
+            <Sidebar
+                notes={notes}
+                onAddNote={onAddNote}
+                onDeleteNote={onDeleteNote}
+                activeNote={activeNote}
+                setActiveNote={setActiveNote}
+                onCheckNote={onCheckNote}
+            />
+            <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
         </div>
     );
 };
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,47 @@
+const Sidebar = ({notes, onAddNote, onDeleteNote, activeNote, setActiveNote, onCheckNote}) => {
+    const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
+    return (
+        <div className="app-sidebar">
+            <div className="center">
+                <div className="app-sidebar-header">
+                    <h1>To-Do List Manager</h1>
+                </div>
+                <button onClick={onAddNote}>Add Note</button>
+            </div>
+            <div className="app-sidebar-notes">
+                {sortedNotes.map(({id, title, body, lastModified}, i) => (
+                    <div
+                        className={`app-sidebar-note ${id === activeNote && "active"}`}
+                        onClick={() => setActiveNote(id)}>
+                        <div className="sidebar-note-title">
+                            <strong
+                                className={`title-name ${sortedNotes[i].completed ? "completed" : ""}`}
+                            >{title && title.substr(0, 10)}</strong>
+                            <div className={"wrap-delete-and-check"}>
+                                <button
+                                    className={"check-btn"}
+                                    onClick={() => onCheckNote(sortedNotes[i])}>Check
+                                </button>
+                                <button
+                                    className={"delete-btn"}
+                                    onClick={() => onDeleteNote(id)}>Delete
+                                </button>
+                            </div>
+                        </div>
+                        <p className={`title-description ${sortedNotes[i].completed ? "completed" : ""}`}
+                        >{body && body.substr(0, 35) + "..."}</p>
+                        <small className="note-meta">
+                            {new Date(lastModified).toLocaleDateString("en-GB", {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                            })}
+                        </small>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
